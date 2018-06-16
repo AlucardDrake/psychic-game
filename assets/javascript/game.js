c8327cc81ef6d1664b47695e349d06580215297f
Texas_ <marshalltheis@gmail.com>
2018-06-16 10:43:03 -0400

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,8 +2,9 @@ var firstRow = ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=",
 var secondRow = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", ""];
 var thirdRow = ["CapsLock", "a", "b", "c", "d", "f", "g", "h", "j", "k", "l", ";", "'", "Enter"];
 var fourthRow = ["Shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "Shift"];
+var fithRow = ["Unidentified","Control","Meta","Alt","Space","Alt","PrintScreen","Control","PageUp","ArrowLeft","ArrowUp","ArrowDown","ArrowRight","PageDown"];
 
-var pickRow = (Math.floor(Math.random() * 4));
+var pickRow = (Math.floor(Math.random() * 5));
 if (pickRow === 0) {
     pickLetter = firstRow[Math.floor(Math.random() * firstRow.length)];
 }
@@ -16,6 +17,9 @@ if (pickRow === 2) {
 if (pickRow === 3) {
     pickLetter = fourthRow[Math.floor(Math.random() * fourthRow.length)];
 }
+if (pickRow === 4) {
+    pickLetter = fithRow[Math.floor(Math.random() * fithRow.length)];
+}
 var pickLetter;
 var userGuess;
 var userWins = 0;
@@ -26,7 +30,7 @@ var html;
 var playAgain = "You win! Press Another Key to Play AGAIN!";
 
 document.onkeyup = function (event) {
-    if (counter > 1) {
+    if (counter > 0) {
         console.log(pickLetter);
         console.log(pickRow);
         userGuess = event.key;
@@ -36,10 +40,11 @@ document.onkeyup = function (event) {
             counter = 0;
             guesses = [];
             document.querySelector("#playAgain").innerHTML = playAgain;
-
-        } else {
-            userLosses++;
+        } else if (pickLetter !== userGuess){
             counter--;
+            if (counter === 0) {
+                userLosses++;
+            }
         }
     } else {
         counter = 5;
@@ -59,9 +64,9 @@ document.onkeyup = function (event) {
         }
         document.querySelector("#playAgain").innerHTML = "";
     }
-    html = "<h1>The Psychic Game</h1>" +
-        "<p>Guess what letter I'm thinking of, its on the #" + pickRow +
-        " row of the keyboard (row #0 is the numbers row)</p>" +
+    html = "<div><h1>The Psychic Game</h1></div>" +
+        "<p>Guess what letter I'm thinking of, its on the #" + (pickRow+1) +
+        " row of the keyboard (row #1 is the numbers row)</p>" +
         "<p>You chose: " + userGuess + "</p>" +
         "<p>wins: " + userWins + "</p>" +
         "<p>losses: " + userLosses + "</p>" +
